669f5b1a38c59cae751752bb34f81d16008959cc
Hiram Guerrero <hiramg90@gmail.com>
2016-11-29 22:18:12 -0600

    categories full working

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -14,6 +14,12 @@
             {!! Form::submit('Update Category', ['class'=>'btn btn-primary']) !!}
         </div>
         {!! Form::close() !!}
+
+        {!! Form::open(['method' => 'DELETE', 'action' => ['AdminCategoriesController@destroy', $category->id]]) !!}
+        <div class="form-group">
+            {!! Form::submit('Delete Category', ['class'=>'btn btn-danger']) !!}
+        </div>
+        {!! Form::close() !!}
     </div>
     <div class="col-sm-6">
 
